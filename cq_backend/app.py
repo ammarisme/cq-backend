9e936786c4a969244ec1b5b731df03eb022f440f
ammarisme <ammarisme@users.noreply.github.com>
2022-05-24 19:50:45 +0000

    ✅ Ready to clone and code.

--- cq_backend/app.py
+++ cq_backend/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+cq_backend API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="cq_backend",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://cq_backend.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "ammarisme",
+        "url": "http://cq_backend.com/contact/",
+        "email": "ammarisme@cq_backend.com",
     },
     license_info={
         "name": "The Unlicense",
--- cq_backend/config.py
+++ cq_backend/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="cq_backend",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="cq_backend_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from cq_backend.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export cq_backend_KEY=value
+export cq_backend_KEY="@int 42"
+export cq_backend_KEY="@jinja {{ this.db.uri }}"
+export cq_backend_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+cq_backend_ENV=production cq_backend run
 ```
 
 Read more on https://dynaconf.com
